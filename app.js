13e0f21cdb9aa7efd1e5ea3ef0ae3871e517931f
test1 <test@gmail.com>
2020-03-17 17:08:47 +0900

    send message

--- app.js
+++ app.js
@@ -2,28 +2,47 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var io = require('socket.io')
+var socketServer = require('socket.io')
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require('./src/routes/index');
+var usersRouter = require('./src/routes/users');
 
 var app = express();
 
 // view engind settting
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
-app.set(path.join(__dirname, 'views'), './views');
+app.set('views', path.join(__dirname, 'src/views'));
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'src/public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // socket io set
+var io = socketServer()
 app.set('io', io);
+
+/*
+io.on('connection', (socket) => {
+  console.log('init connection')
+})
+*/
+
+var chatNsp = io.of('/chat')
+
+chatNsp.on('connect', (socket) => {
+  console.log('chat connection');
+
+  chatNsp.emit('init', 'hello')
+  socket.on('send', (msg) => {
+    console.log(`send ${msg}`);
+    chatNsp.emit('receive', msg);
+  });
+});
 
 module.exports = app;
